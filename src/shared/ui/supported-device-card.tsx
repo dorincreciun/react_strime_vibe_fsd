8d2da6693b3d3f429668bfcb74d16ff845ee19f4
dorincreciun <creciundorin25@gmail.com>
2025-12-16 15:44:29 +0200

    feat(pages/homepage): add fake data for supported-device-card.tsx component

---
+++ src/shared/ui/supported-device-card.tsx
@@ -0,0 +1,30 @@
+import type { ReactNode } from "react"
+
+import { Text } from "@shared/ui/text"
+import { Title } from "@shared/ui/title"
+import { cn } from "@shared/utils/cn"
+
+interface SupportedDeviceCardProps {
+    icon: ReactNode
+    title: string
+    description: string
+}
+export const SupportedDeviceCard = ({ icon, title, description }: SupportedDeviceCardProps) => {
+    return (
+        <div
+            className={cn([
+                "border-black-15 border",
+                "max-xl:rounded-[10px] xl:rounded-xl",
+                "max-xl:p-10 max-md:p-6 xl:p-[50px]",
+            ])}
+        >
+            <div className={"flex flex-col max-xl:gap-6 max-md:gap-5 xl:gap-[30px]"}>
+                <div className="flex items-center max-xl:gap-3 max-md:gap-2.5 xl:gap-4">
+                    <div>{icon}</div>
+                    <Title size="sm">{title}</Title>
+                </div>
+                <Text>{description}</Text>
+            </div>
+        </div>
+    )
+}
